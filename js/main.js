82bdc7b49724ba918ace42125200c3c5f9c156c8
Roxy Garcia <roxy3d1988@gmail.com>
2019-08-14 20:44:19 -0700

    finished take home day 2

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 
 //example of const
 //how to check length of string
-const firstName = 'Roxy';
-console.log(firstName.length);
+// const firstName = 'Roxy';
+// console.log(firstName.length);
 // firstName = 'Roxana';
 
 //old way
 // console.log('My name is ' + firstName + ' and my age is ' + age);
 
-//better way....use this is class....add back ticks when you introduce variables
+//better way....use this in class....add back ticks when you introduce variables
 // console.log(`My name is ${firstName} and my age is ${age}`);
 
 // alert(`Hello, I am ${firstName}!`);
@@ -22,6 +22,21 @@ console.log(firstName.length);
 // let hello = new String('Hello, there.');
 // alert(hello);
 
-const topThreeMovies = ['Lion King', 'A Star is Born', 'Ratatouille'];
-console.log(topThreeMovies);
-console.log(`my second top movie is: ${topThreeMovies[1]}`);
+// const topThreeMovies = ['Lion King', 'A Star is Born', 'Ratatouille'];
+// console.log(topThreeMovies);
+// console.log(`my second top movie is: ${topThreeMovies[1]}`);
+
+
+// Take home - Day 2
+
+const questionFirst = prompt('What is your first name?');
+const questionLast = prompt('What is your last name?');
+
+console.log(`Your first name is ${questionFirst}.`);
+alert(`Your last name is ${questionLast}.`);
+
+const questionBday = prompt('When is your birthday?');
+confirm(`Is your birthday ${questionBday}?`);
+
+alert(`Thank you for confirming. Your birthday is ${new Date (questionBday)}.`);
+
