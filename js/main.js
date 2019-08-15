d1b629ae2a092ae965f2a4493fc5bc4d40fd6d2e
Roxy Garcia <roxy3d1988@gmail.com>
2019-08-14 19:54:35 -0700

    more about let, var, const, and console.log

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// var firstName = 'Roxy';
+// var age = 31;
+
+//example of const
+//how to check length of string
+const firstName = 'Roxy';
+console.log(firstName.length);
+// firstName = 'Roxana';
+
+//old way
+// console.log('My name is ' + firstName + ' and my age is ' + age);
+
+//better way....use this is class....add back ticks when you introduce variables
+// console.log(`My name is ${firstName} and my age is ${age}`);
+
+// alert(`Hello, I am ${firstName}!`);
+
+// const birthday = '06/06/1988';
+// let fixDate = new Date(birthday);
+// console.log(fixDate);
+
+// let hello = new String('Hello, there.');
+// alert(hello);
+
+const topThreeMovies = ['Lion King', 'A Star is Born', 'Ratatouille'];
+console.log(topThreeMovies);
+console.log(`my second top movie is: ${topThreeMovies[1]}`);
